0506123ffb1efa9d5b70eff21726b5d9082f5bb6
hamed911 <hamed_gorjiara@yahoo.com>
2016-05-04 22:54:45 +0430

    login replacement is added

--- UI/js/app.js
+++ UI/js/app.js
@@ -4,4 +4,16 @@
  * and open the template in the editor.
  */
 
-
+(function (){
+    var app = angular.module('stockmarket',[]);
+    app.controller('MarketController',function ($scope){
+        var marketCtrl = this;
+        $scope.session = null;
+        this.doLogin = function(){
+            $scope.session= marketCtrl.users [$scope.enteredID];
+        }
+        this.users ={
+            "123":{'name':'hamed','money':'2000'}
+        };
+    });
+})();
