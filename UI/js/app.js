24c95292e4f98353676f95f22d6bae88114a2b59
hamed911 <hamed_gorjiara@yahoo.com>
2016-05-05 02:06:23 +0430

    Market Status (part 2) is implemented. Also, update button is added.

--- UI/js/app.js
+++ UI/js/app.js
@@ -8,12 +8,23 @@
     var app = angular.module('stockmarket',[]);
     app.controller('MarketController',function ($scope){
         var marketCtrl = this;
-        $scope.session = null;
+        this.session = null;
+        this.symbols=[];
         this.doLogin = function(){
             $scope.session= marketCtrl.users [$scope.enteredID];
         }
         this.users ={
             "123":{'name':'hamed','money':'2000'}
         };
+        var dataSymbol = [{
+            "name":"Rana",
+            "quantity": 200,
+            "sellingOffers":[{"id":22,"quantity":20,"price":240,"type":"GTC"},{"id":22,"quantity":20,"price":240,"type":"GTC"}],
+            "buyingOffers":[{"id":22,"quantity":20,"price":240,"type":"GTC"},{"id":22,"quantity":20,"price":240,"type":"GTC"}]
+        }];
+        this.symbols = dataSymbol;
+        this.updateSymbols = function(){
+            alert('not implemented');
+        }
     });
 })();
